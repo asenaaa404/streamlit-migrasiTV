d976c2d3f8076d56534fce72863f1bd29f59d934
asenaaa404 <151552011+asenaaa404@users.noreply.github.com>
2023-11-22 20:34:10 +0700

    Update app_sentimen_bilstm.py

--- app_sentimen_bilstm.py
+++ app_sentimen_bilstm.py
@@ -11,10 +11,10 @@ from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFacto
 import subprocess
 
 # Load model
-model = load_model("model/sentiment_model.h5")
+model = load_model("sentiment_model.h5")
 
 # Load Tokenizer
-with open("model/sentiment_tokenizer.pkl", "rb") as tokenizer_file:
+with open("sentiment_tokenizer.pkl", "rb") as tokenizer_file:
     tokenizer = pickle.load(tokenizer_file)
 
 # Fungsi Preprocessing
